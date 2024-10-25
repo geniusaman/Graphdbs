85559d170895829eea9bf8ad65625aceb4fa2e8f
Aman Gupta <108857118+geniusaman@users.noreply.github.com>
2024-10-25 18:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ schema = enhanced_graph.schema
 
 # Initialize LLM
 groq_llm = ChatGroq(
-    model="llama-3.1-70b-versatile",
+    model="llama-3.1-8b-instant",
 )
 # co = cohere.Client('Cohere-Api-Key')
 open_llm = ChatOpenAI(model="gpt-3.5-turbo")
