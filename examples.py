e21054bd88ef139278bb7cd92c213acee508ff52
Aman Gupta <108857118+geniusaman@users.noreply.github.com>
2024-10-28 17:28:08 +0530

    Update examples.py

--- examples.py
+++ examples.py
@@ -159,7 +159,7 @@ examples = [
  
     },
     {   "question":"Is there any delayed request for this 316 Stainless Square Bar product",
-         "query":"MATCH (e:Email) WHERE e.subject CONTAINS 'PO Change Approval Request' AND e.mailBody CONTAINS 'PO Change Approval Request' AND e.originalDeliveryDate IS NOT NULL AND e.requestedDeliveryDate IS NOT NULL WITH e MATCH (po:PurchaseOrder) WHERE po.product_name CONTAINS '316 Stainless Square Bar 1' AND po.po_number = e.poNumber RETURN DISTINCT e.poNumber as PO_Number, e.originalDeliveryDate as Original_Delivery_Date, e.requestedDeliveryDate as Requested_Delivery_Date, e.sender as Sender, e.recipient as Recipient, e.sentDate as Email_Sent_Date, po.product_name as Product_Name, e.mailBody as Mail_Body;",
+         "query":"MATCH (e:Email) WHERE e.subject CONTAINS 'PO Change Approval Request' AND e.mailBody CONTAINS 'PO Change Approval Request' AND e.originalDeliveryDate IS NOT NULL AND e.requestedDeliveryDate IS NOT NULL WITH e MATCH (po:PurchaseOrder) WHERE po.product_name CONTAINS '316 Stainless Square Bar 1' AND po.po_number = e.poNumber RETURN DISTINCT e.poNumber as PO_Number, e.originalDeliveryDate as Original_Delivery_Date, e.requestedDeliveryDate as Requested_Delivery_Date, e.sender as Sender, e.recipient as Recipient, e.sentDate as Email_Sent_Date, po.product_name as Product_Name;",
  
     },
     {   "question":"Is there any potential low stock alert for product 316 Stainless Square Bar",
